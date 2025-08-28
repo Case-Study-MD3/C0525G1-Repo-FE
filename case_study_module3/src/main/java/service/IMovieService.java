04317cbbecf65e3146dc4e5e9af9521c39f3583d
VanThanh34 <vvt1304@gmail.com>
2025-08-28 09:05:41 +0700

    sai

--- case_study_module3/src/main/java/service/IMovieService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import entity.Movie;
-import java.util.Map;
-
-public interface IMovieService {
-    Map<String, Movie> getAllMovies();
-    Movie getMovieByTitle(String title);
-}
